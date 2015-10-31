d2e1034cf5a9cbb71213e112e500d462695b9437
huazhu111 <luodadi.huazhu@gmail.com>
2015-10-31 12:11:27 +0800

    First Version

---
+++ FDJ/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace FDJ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.currentTimeLabel = new System.Windows.Forms.Label();
+            this.totalTimeLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(279, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 50, 0, 50);
+            this.label1.Size = new System.Drawing.Size(154, 120);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "将文件拖动到这里";
+            this.label1.DragDrop += new System.Windows.Forms.DragEventHandler(this.label1_DragDrop);
+            this.label1.DragEnter += new System.Windows.Forms.DragEventHandler(this.label1_DragEnter);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(134, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 71);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "播放";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(406, 301);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 71);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "暂停";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(77, 425);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 64);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "无";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(192, 425);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 64);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "无";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(307, 425);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 64);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "无";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(426, 425);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(85, 64);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "无";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(538, 425);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(85, 64);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "无";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(279, 513);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(140, 63);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "清空";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 200;
+            this.trackBar1.Location = new System.Drawing.Point(134, 200);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(455, 69);
+            this.trackBar1.SmallChange = 100;
+            this.trackBar1.TabIndex = 9;
+            this.trackBar1.TickFrequency = 0;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // currentTimeLabel
+            // 
+            this.currentTimeLabel.AutoSize = true;
+            this.currentTimeLabel.Location = new System.Drawing.Point(131, 262);
+            this.currentTimeLabel.Name = "currentTimeLabel";
+            this.currentTimeLabel.Size = new System.Drawing.Size(48, 18);
+            this.currentTimeLabel.TabIndex = 10;
+            this.currentTimeLabel.Text = "00:00";
+            // 
+            // totalTimeLabel
+            // 
+            this.totalTimeLabel.AutoSize = true;
+            this.totalTimeLabel.Location = new System.Drawing.Point(546, 262);
+            this.totalTimeLabel.Name = "totalTimeLabel";
+            this.totalTimeLabel.Size = new System.Drawing.Size(48, 18);
+            this.totalTimeLabel.TabIndex = 11;
+            this.totalTimeLabel.Text = "00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label2.Location = new System.Drawing.Point(526, 706);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 18);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Ver 0.0.2 By Yuki Dev.";
+            // 
+            // button9
+            // 
+            this.button9.Enabled = false;
+            this.button9.Location = new System.Drawing.Point(134, 616);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(145, 59);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "A";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click_1);
+            this.button9.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button9_MouseDown);
+            // 
+            // button10
+            // 
+            this.button10.Enabled = false;
+            this.button10.Location = new System.Drawing.Point(426, 616);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(153, 59);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "B";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click_1);
+            this.button10.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button10_MouseDown);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(25, 28);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(136, 54);
+            this.button11.TabIndex = 15;
+            this.button11.Text = "帮助";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 751);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.totalTimeLabel);
+            this.Controls.Add(this.currentTimeLabel);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "复读机";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label currentTimeLabel;
+        private System.Windows.Forms.Label totalTimeLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+
+    }
+}
+
---
+++ FDJ/Form1.cs
@@ -0,0 +1,536 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices; 
+
+namespace FDJ
+{
+    public partial class Form1 : Form
+    {
+        string[] breaklist = new string[5] { "0", "0", "0", "0", "0" };
+        clsMCI cm = new clsMCI();
+        string a = "0", b = "0";
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void addlist(string newtime)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                breaklist[i] = breaklist[i + 1];
+            }
+            breaklist[4] = newtime;
+            button3.Text = (breaklist[0] == "0") ? "无" : formatLongToTimeStr(long.Parse(breaklist[0]));
+            button4.Text = (breaklist[1] == "0") ? "无" : formatLongToTimeStr(long.Parse(breaklist[1]));
+            button5.Text = (breaklist[2] == "0") ? "无" : formatLongToTimeStr(long.Parse(breaklist[2]));
+            button6.Text = (breaklist[3] == "0") ? "无" : formatLongToTimeStr(long.Parse(breaklist[3]));
+            button7.Text = (breaklist[4] == "0") ? "无" : formatLongToTimeStr(long.Parse(breaklist[4]));
+        }
+        public static String formatLongToTimeStr(long l)
+        {
+            String str = "aaa";
+            int hour = 0;
+            int minute = 0;
+            int second = 0;
+            int ms = 0;
+            second = (int)l / 1000;
+            ms = (int)l - second * 1000;
+            if (second > 60)
+            {
+                minute = second / 60;
+                second = second % 60;
+            }
+            if (minute > 60)
+            {
+                hour = minute / 60;
+                minute = minute % 60;
+            }
+            return string.Format("{0}:{1}:{2}",hour.ToString(), minute.ToString(), second.ToString());
+        }
+        public class clsMCI
+        {
+
+            public clsMCI()
+            {
+
+                //
+
+                // TODO: 在此处添加构造函数逻辑
+
+                //
+
+            }
+
+
+
+            //定义API函数使用的字符串变量 
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+
+            private string Name = "";
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+
+            private string durLength = "";
+
+            [MarshalAs(UnmanagedType.LPTStr, SizeConst = 128)]
+
+            private string TemStr = "";
+
+            int ilong;
+
+            //定义播放状态枚举变量
+
+            public enum State
+            {
+
+                mPlaying = 1,
+
+                mPuase = 2,
+
+                mStop = 3
+
+            };
+
+            //结构变量
+
+            public struct structMCI
+            {
+
+                public bool bMut;
+
+                public int iDur;
+
+                public int iPos;
+
+                public int iVol;
+
+                public int iBal;
+
+                public string iName;
+
+                public State state;
+
+            };
+
+
+
+            public structMCI mc = new structMCI();
+            public int id;
+
+
+            //取得播放文件属性
+
+            public string FileName
+            {
+
+                get
+                {
+
+                    return mc.iName;
+
+                }
+
+                set
+                {
+
+                    //ASCIIEncoding asc = new ASCIIEncoding(); 
+
+                    try
+                    {
+                        id++;
+
+                        //if(mc.state == State.mPlaying) this.StopT();
+
+                        Name = "";
+
+                        TemStr = "";
+
+                        TemStr = TemStr.PadLeft(127, Convert.ToChar(" "));
+
+                        Name = Name.PadLeft(260, Convert.ToChar(" "));
+
+                        mc.iName = value;
+
+                        ilong = APIClass.GetShortPathName(mc.iName, Name, Name.Length);
+
+                        Name = GetCurrPath(Name);
+
+                        //Name = "open " + Convert.ToChar(34) + Name + Convert.ToChar(34) + " alias " + id.ToString() + "";
+
+                        Name = "open " + Name + " alias " + id.ToString();
+
+                        ilong = APIClass.mciSendString("close all", TemStr, TemStr.Length, 0);
+
+                        ilong = APIClass.mciSendString(Name, TemStr, TemStr.Length, 0);
+
+                        if (ilong != 0) {
+
+                            StringBuilder errorText = new StringBuilder();
+                            APIClass.mciGetErrorString(ilong, errorText, 50);
+
+                            MessageBox.Show(errorText.ToString());
+                        }
+
+                        ilong = APIClass.mciSendString("set " + id.ToString() + " time format milliseconds", TemStr, TemStr.Length, 0);
+
+                        mc.state = State.mStop;
+
+                    }
+
+                    catch
+                    {
+
+                    }
+
+                }
+
+            }
+
+            //播放
+
+            public void play()
+            {
+
+                TemStr = "";
+
+                TemStr = TemStr.PadLeft(127, Convert.ToChar(" "));
+
+                APIClass.mciSendString("play " + id.ToString(), TemStr, TemStr.Length, 0);
+
+                mc.state = State.mPlaying;
+
+            }
+
+            //停止
+
+            public void StopT()
+            {
+
+                TemStr = "";
+
+                TemStr = TemStr.PadLeft(128, Convert.ToChar(" "));
+
+                ilong = APIClass.mciSendString("close " + id.ToString(), TemStr, 128, 0);
+
+                ilong = APIClass.mciSendString("close all", TemStr, 128, 0);
+
+                mc.state = State.mStop;
+
+            }
+
+
+
+            public void Puase()
+            {
+
+                TemStr = "";
+
+                TemStr = TemStr.PadLeft(128, Convert.ToChar(" "));
+
+                ilong = APIClass.mciSendString("pause " + id.ToString(), TemStr, TemStr.Length, 0);
+
+                mc.state = State.mPuase;
+
+            }
+
+            private string GetCurrPath(string name)
+            {
+
+                if (name.Length < 1) return "";
+
+                name = name.Trim();
+
+                name = name.Substring(0, name.Length - 1);
+
+                return name;
+
+            }
+
+            //总时间
+
+            public string Duration
+            {
+
+                get
+                {
+
+                    durLength = "";
+
+                    durLength = durLength.PadLeft(128, Convert.ToChar(" "));
+
+                    APIClass.mciSendString("status " + id.ToString() + " length", durLength, durLength.Length, 0);
+
+                    durLength = durLength.Trim();
+
+                    if (durLength == "") return "null";
+
+                    return durLength;
+
+                }
+
+            }
+
+            public int seek(int position)
+            {
+                APIClass.mciSendString(string.Format("seek " + id.ToString() + " to {0}",position.ToString()),"",0,0);
+                this.play();
+                return 1;
+            }
+            public int seekwithoutplay(int position)
+            {
+                APIClass.mciSendString(string.Format("seek " + id.ToString() + " to {0}", position.ToString()), "", 0, 0);
+                return 1;
+            }
+
+            //当前时间
+
+            public string CurrentPosition
+            {
+
+                get
+                {
+
+                    durLength = "";
+
+                    durLength = durLength.PadLeft(128, Convert.ToChar(" "));
+
+                    APIClass.mciSendString("status " + id.ToString() + " position", durLength, durLength.Length, 0);
+
+                    //mc.iPos = (int)(Convert.ToDouble(durLength) / 1000f);
+
+                    return durLength.Trim();
+
+                }
+
+            }
+
+        }
+
+
+
+        public class APIClass
+        {
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+
+            public static extern int GetShortPathName(
+
+            string lpszLongPath,
+
+            string shortFile,
+
+            int cchBuffer
+
+            );
+
+
+
+            [DllImport("winmm.dll", EntryPoint = "mciSendString", CharSet = CharSet.Auto)]
+
+            public static extern int mciSendString(
+
+            string lpstrCommand,
+
+            string lpstrReturnString,
+
+            int uReturnLength,
+
+            int hwndCallback
+
+            );
+            [DllImport("winmm.dll")]
+            public static extern bool mciGetErrorString(Int32 errorCode, StringBuilder errorText, Int32 errorTextSize);
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_DragDrop(object sender, DragEventArgs e)
+        {
+            timer1.Enabled = false;
+            label1.Text = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            cm.FileName = label1.Text;
+            totalTimeLabel.Text = formatLongToTimeStr(long.Parse(cm.Duration));
+            trackBar1.Maximum = int.Parse(cm.Duration);
+            timer1.Enabled = true;
+            for (int i = 0; i < 5; i++) addlist("0");
+            button1.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button9.Enabled = true;
+            button10.Enabled = true;
+        }
+
+        private void label1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            cm.play();
+            button2.Enabled = true;
+            button1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cm.Puase();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            addlist(cm.CurrentPosition);
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            cm.seek(int.Parse(breaklist[0]));
+            if (button1.Enabled == true)
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            cm.seek(int.Parse(breaklist[1]));
+            if (button1.Enabled == true)
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            cm.seek(int.Parse(breaklist[2]));
+            if (button1.Enabled == true)
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            cm.seek(int.Parse(breaklist[3]));
+            if (button1.Enabled == true)
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            cm.seek(int.Parse(breaklist[4]));
+            if (button1.Enabled == true)
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++) addlist("0");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            trackBar1.Value = int.Parse(cm.CurrentPosition);
+            currentTimeLabel.Text = formatLongToTimeStr(long.Parse(cm.CurrentPosition));
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (cm.mc.state == clsMCI.State.mPlaying)
+            {
+                cm.seek(trackBar1.Value);
+            }
+            else
+            {
+                cm.seekwithoutplay(trackBar1.Value);
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            cm.FileName = "C:\\Users\\Yukimir\\Music\\1.mp3";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            cm.FileName = "C:\\Users\\Yukimir\\Music\\2.mp3";
+        }
+
+        private void button9_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                a = cm.CurrentPosition;
+                button9.Text = formatLongToTimeStr(long.Parse(a));
+            }
+        }
+
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            cm.seek(int.Parse(a));
+            if (button1.Enabled == true)
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button10_Click_1(object sender, EventArgs e)
+        {
+            cm.seek(int.Parse(b));
+            if (button1.Enabled == true)
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button10_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                b = cm.CurrentPosition;
+                button10.Text = formatLongToTimeStr(long.Parse(b));
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("暂停时当前进度会在填充进下方的进度槽\n"+
+                            "（当超过五个时，最旧的一个会被删除）\n"+
+                            "点击进度按钮可以从相应的进度开始播放。\n"+
+                            "最下方的A/B按键是自定义记录点，右键点击按键可以记录当前进度\n"+
+                            "左键点击跳至记录的进度");
+        }
+
+    }
+}
